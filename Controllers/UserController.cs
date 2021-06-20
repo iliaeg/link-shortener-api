d82ab87d67749a11cfa535237633f1616f366664
Ilia Egorov <Egorovilyak@yandex.ru>
2021-06-20 10:12:47 +0300

    user controller implemented & json result error helper added

---
+++ Controllers/UserController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+using LinkShortenerAPI.Repositories;
+using LinkShortenerAPI.Models;
+using LinkShortenerAPI.Helpers;
+
+namespace LinkShortenerAPI.Controllers
+{
+    /// <summary>
+    /// User management controller.
+    /// </summary>
+    /// <remarks>For simplicity, uses ObjectId as the authentication token.</remarks>
+    [ApiController]
+    [Route("api/v{version:apiVersion}/users")]
+    [Route("api/users")]
+    [ApiVersion("1.0")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserController"/> class.
+        /// </summary>
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Creates a new user in repository.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (await userRepository.GetByEmail(user.Email) != null)
+            {
+                return new JsonErrorResult("User with specified email already exists.", HttpStatusCode.BadRequest);
+            }
+
+            var id = await userRepository.Create(user);
+            return new JsonResult(id.ToString());
+        }
+
+        /// <summary>
+        /// Authenticates a user.
+        /// </summary>
+        [HttpPost("Auth")]
+        public async Task<IActionResult> Authentication(AuthenticationRequest authenticationRequest)
+        {
+            var user = await userRepository.GetByEmail(authenticationRequest.Email);
+            if (user is null)
+            {
+                return new JsonErrorResult("Specified user not found.", HttpStatusCode.Unauthorized);
+            }
+
+            if (authenticationRequest.Password != user.Password)
+            {
+                return new JsonErrorResult("Password is incorrect.", HttpStatusCode.Unauthorized);
+            }
+
+            return new JsonResult(user.Id.ToString());
+
+        }
+
+        /// <summary>
+        /// Returns short info about the api.
+        /// </summary>
+        [HttpGet("About")]
+        public ContentResult About()
+        {
+            return Content("An API for registering users in the service.");
+        }
+
+        // Admin/test tools
+
+        /// <summary>
+        /// Gets user by id.
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            if (!ObjectId.TryParse(id, out ObjectId objectId))
+            {
+                return new JsonErrorResult("User id is invalid.", HttpStatusCode.BadRequest);
+            }
+
+            var user = await userRepository.Get(objectId);
+            return new JsonResult(user);
+        }
+
+        /// <summary>
+        /// Gets user by email.
+        /// </summary>
+        [HttpGet("ByEmail/{email}")]
+        public async Task<IActionResult> GetByEmail(string email)
+        {
+            if (email is null)
+            {
+                return new JsonErrorResult("Email is null.", HttpStatusCode.BadRequest);
+            }
+
+            var user = await userRepository.GetByEmail(email);
+            return new JsonResult(user);
+        }
+    }
+}
---
+++ Helpers/JsonErrorResult.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkShortenerAPI.Helpers
+{
+    /// <summary>
+    /// Аllows to return an error in the json format with a status code.
+    /// </summary>
+    public class JsonErrorResult : JsonResult
+    {
+        private readonly HttpStatusCode statusCode;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonErrorResult"/> class.
+        /// </summary>
+        /// <param name="json">Json object with error parameters.</param>
+        /// <remarks>Status code is not specified, so it will be equals 500.</remarks>
+        public JsonErrorResult(object json)
+            : this(json, HttpStatusCode.InternalServerError)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonErrorResult"/> class.
+        /// </summary>
+        /// <param name="json">Json object with error parameters.</param>
+        /// <param name="statusCode">Http status code of the error.</param>
+        public JsonErrorResult(object json, HttpStatusCode statusCode)
+            : base(json)
+        {
+            this.statusCode = statusCode;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonErrorResult"/> class.
+        /// </summary>
+        /// <param name="errorMessage">Error message.</param>
+        /// <param name="statusCode">Http status code of the error.</param>
+        public JsonErrorResult(string errorMessage, HttpStatusCode statusCode)
+            : base(new { error = errorMessage })
+        {
+            this.statusCode = statusCode;
+        }
+
+        /// <inheritdoc/>
+        public override void ExecuteResult(ActionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int)statusCode;
+            base.ExecuteResult(context);
+        }
+
+        /// <inheritdoc/>
+        public override Task ExecuteResultAsync(ActionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int)statusCode;
+            return base.ExecuteResultAsync(context);
+        }
+    }
+}
